3fd8d36ed9a4360484d63d3fb2dba7ab5a8a2964
Yomi Eluwande <iamlese@yahoo.com>
2017-08-10 19:58:49 +0100

    removed document.write bug

--- src/routes/home/index.js
+++ src/routes/home/index.js
@@ -10,10 +10,9 @@ export default class Home extends Component {
 		};
 	}
 
-	componentDidMount () {
+	componentWillMount () {
 		axios.get('https://api.dribbble.com/v1/shots?list=debuts&sort=recent&page=2&per_page=12&access_token=14d01cc0327dfd5900044a4b70978e4f90f24d5db82b38421e522cbd35f16d6d')
 			.then( res => {
-				console.log(res.data);
 				this.setState({ shots: res.data });
 			})
 			.catch(err => {
@@ -29,7 +28,7 @@ export default class Home extends Component {
                             <div class="col-xs-12 col-sm-6 col-md-4" key={i}>
                                 <div class={style.box}>
                                     <a href={item.html_url} target="_blank" class={style.title}>
-                                        <img src={item.images.hidpi ||item.images.normal || item.images.teaser} />
+                                        <img src={item.images.hidpi || item.images.normal || item.images.teaser} />
                                     </a>
                                     <a href={item.html_url} target="_blank" class={style.title}>{item.title}</a>
                                     <div class={style.userprofile}>
